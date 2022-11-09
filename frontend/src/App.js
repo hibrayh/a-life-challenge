9d3311414a011ee1d76ba31bb90c83342a53c7de
Josh Corbin <corbinj@oregonstate.edu>
2022-11-08 18:53:24 -0800

    testing out React with buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const Intro = (props) => {
+  return (
+    <>
+    <h1>Intro: {props.text}</h1>
+    </>
+  )
+}
+const StartButton = () => {
+  return (
+    <div>
+      <button id="menuButtonStart">Start</button>
+    </div>
+  )
+}
+
+const MenuButtonQuit = () => {
+  return (
+    <button id="menuButtonQuit">Quit</button>
+  )
+}
+
 function App() {
   return (
     <div>
       <header class="menu">
-      <h1>A-Life Challenge!</h1>
+        <Intro text={"testing"} />
       </header>
+      <div class="menu">
+      <div id="testAnimation"></div> 
+      <StartButton />
+      <MenuButtonQuit />
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
