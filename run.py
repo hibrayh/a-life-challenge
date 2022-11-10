e2c8010633c5135b5ebf755bda8ef2fd58868e20
lowebr <lowebr@oregonstate.edu>
2022-11-09 18:30:10 -0800

    Revised build script
    Further revised build script to account for subprocess treatment differences across Linux and Windows

--- run.py
+++ run.py
@@ -8,12 +8,24 @@ def runApp():
     # Change working directory
     os.chdir("frontend/")
     # Start React app
-    react_app = subprocess.Popen(["npm", "start"], shell=True)
+    react_app = 0
+    if os.name == 'nt':
+        react_app = subprocess.Popen(["npm", "start"], shell=True)
+    else:
+        react_app = subprocess.Popen(["npm", "start"])
+
     # Give React time to intitialize before starting electron
     time.sleep(5)
+
     # Start Electron
-    electron_app = subprocess.Popen(["npm", "run", "electron-dev"], shell=True)
+    electron_app = 0
+    if os.name == 'nt':
+        electron_app = subprocess.Popen(["npm", "run", "electron-dev"], shell=True)
+    else:
+        electron_app = subprocess.Popen(["npm", "run", "electron-dev"])
+    
     electron_app.wait()
+
     # Kill the React app
     print("Killing react app")
     if os.name == 'nt':
