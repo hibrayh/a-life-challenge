fe675934bcb392098ad63154e749644ef8f05bcd
lowebr <lowebr@oregonstate.edu>
2022-11-15 04:28:44 +0000

    Autoformatter changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,59 @@
-
-import { useState } from 'react';
-import './App.css';
-
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  const [showMenu, setShowMenu] = useState(true);
-  const [showSimulation, setShowSimulation] = useState(false);
+    const [showMenu, setShowMenu] = useState(true)
+    const [showSimulation, setShowSimulation] = useState(false)
 
-  const Menu = () => {
-    return(
-      <>
-      <header className="menu">
-        <h1>A-Life Challenge</h1>
-      </header>
-      <div class="menu">
-  
-        <div>
-          <button id="menuButtonStart" onClick={() => {setShowMenu(false); setShowSimulation(true)}}>Start</button>
-        </div>
-  
-        <div>
-          <button id="menuButtonQuit">Quit</button>
-        </div>
-  
-      </div>
-    </>
-    )
-  
-  }
-  
-  // "Page" that will show the simulation
-  const Simulation = () => {
-    return(
-      <>
-        <header className="menu">
-          <h1>New page</h1>
-        </header>
-      </>
-    )
-  
-  }
+    const Menu = () => {
+        return (
+            <>
+                <header className="menu">
+                    <h1>A-Life Challenge</h1>
+                </header>
+                <div class="menu">
+                    <div>
+                        <button
+                            id="menuButtonStart"
+                            onClick={() => {
+                                setShowMenu(false)
+                                setShowSimulation(true)
+                            }}>
+                            Start
+                        </button>
+                    </div>
 
-  return (
-    <>
-      {
-        //Apparently this is how you comment in react, need to have the {}
-        /* This is a multi-line comment, also needs the {} 
+                    <div>
+                        <button id="menuButtonQuit">Quit</button>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    // "Page" that will show the simulation
+    const Simulation = () => {
+        return (
+            <>
+                <header className="menu">
+                    <h1>New page</h1>
+                </header>
+            </>
+        )
+    }
+
+    return (
+        <>
+            {
+                //Apparently this is how you comment in react, need to have the {}
+                /* This is a multi-line comment, also needs the {} 
         if showMenu = true, display the menu. If showSimulation = true, show the simulation.
         */
-      }
-      {showMenu ? 
-        (<Menu />)
-        : null
-      }
-      {showSimulation ? 
-        (<Simulation />)
-        : null
-      }
-      
-    </>
-  )
+            }
+            {showMenu ? <Menu /> : null}
+            {showSimulation ? <Simulation /> : null}
+        </>
+    )
 }
-
 
 export default App
