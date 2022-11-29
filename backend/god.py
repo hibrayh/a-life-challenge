5e2fa1ba5d30e5f2bb7c609d6e263275ce9261d3
lowebr <lowebr@oregonstate.edu>
2022-11-28 21:37:56 -0800

    Added creature/species creation/deletion routines to backend API

--- backend/god.py
+++ backend/god.py
@@ -1,4 +1,5 @@
 import logging
+import environment
 import creatures.species_manager
 
 logging.basicConfig(
@@ -11,27 +12,81 @@ class God:
         logging.info("Initializing new God object")
 
         self._speciesManagers = []
+        self._environment = environment.Environment()
+    
+    def _getSpeciesManagerFromName(self, speciesName):
+        speciesManagerOfInterest = None
+
+        for speciesManager in speciesManagerOfInterest:
+            if speciesManager.speciesName == speciesName:
+                speciesManagerOfInterest = speciesManager
+                break
+        
+        return speciesManagerOfInterest
 
     def createNewSpecies(self, speciesName, startingGenome):
-        pass
+        logging.info(f"Creating new species: {speciesName}")
+
+        newSpecies = creatures.species_manager.SpeciesManager(speciesName, startingGenome)
+        self._speciesManagers.append(newSpecies)
 
     def deleteSpecies(self, speciesName):
-        pass
+        logging.info(f"Deleting species: {speciesName}")
+
+        speciesManagerToDelete = self._getSpeciesManagerFromName(speciesName)
+        
+        if speciesManagerToDelete == None:
+            logging.info("Could not find species to delete")
+        else:
+            self._speciesManagers.remove(speciesManagerToDelete)
 
     def editSpeciesGenome(self, speciesName, newGenome):
-        pass
+        speciesManagerToEdit = self._getSpeciesManagerFromName(speciesName)
+
+        if speciesManagerToEdit is None:
+            logging.info("Could not find species to edit")
+        else:
+            speciesManagerToEdit.editSpeciesGenome(newGenome)
 
     def renameSpecies(self, originalSpeciesName, newSpeciesName):
-        pass
+        speciesManagerToRename = self._getSpeciesManagerFromName(originalSpeciesName)
+
+        if speciesManagerToRename is None:
+            logging.info("Could not find species to rename")
+        else:
+            speciesManagerToRename.renameSpecies(newSpeciesName)
+
+    def addSpeciesRelationship(self, speciesOfInterest, newSpecies, newRelationship):
+        speciesManagerToAddRelation = self._getSpeciesManagerFromName(speciesOfInterest)
+
+        if speciesManagerToAddRelation is None:
+            logging.info("Could not find species to add relationship to")
+        else:
+            speciesManagerToAddRelation.addSpeciesRelationship(newSpecies, newRelationship)
 
     def createNewCreature(self, speciesName, startingGenome):
-        pass
+        speciesManagerOfInterest = self._getSpeciesManagerFromName(speciesName)
 
-    def deleteCreature(self, creatureId):
-        pass
+        if speciesManagerOfInterest is None:
+            logging.info("Could not find species to add creature to")
+        else:
+            speciesManagerOfInterest.createNewCreature(startingGenome)
 
-    def editCreatureGenome(self, creatureId):
-        pass
+    def deleteCreature(self, speciesName, creatureId):
+        speciesManagerOfInterest = self._getSpeciesManagerFromName(speciesName)
+
+        if speciesManagerOfInterest is None:
+            logging.info("Could not find species to delete creature from")
+        else:
+            speciesManagerOfInterest.deleteCreature(creatureId)
+
+    def editCreatureGenome(self, speciesName, creatureId, newGenome):
+        speciesManagerOfInterest = self._getSpeciesManagerFromName(speciesName)
+
+        if speciesManagerOfInterest is None:
+            logging.info("Could not find species to edit creature")
+        else:
+            speciesManagerOfInterest.editCreatureGenome(creatureId, newGenome)
 
     def getSpeciesInfo(self, speciesName):
         pass
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,55 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+import god
+import creatures.genome
 
 api = Flask(__name__)
 cors = CORS(api)
 api.config["CORS_HEADERS"] = "Content-Type"
+
+GOD = None
+
+def _convertRequestToGenome(request):
+    receptorsList = []
+
+    if request.form['canSee'] == 'true':
+        receptorsList.append(creatures.genome.Receptors.VISION)
+    if request.form['canSmell'] == 'true':
+        receptorsList.append(creatures.genome.Receptors.SMELL)
+    if request.form['canHear'] == 'true':
+        receptorsList.append(creatures.genome.Receptors.HEAR)
+    
+    inputGenome = creatures.genome.Genome(int(request.form['visibility']),
+        int(request.form['maxHealth']),
+        receptorsList,
+        int(request.form['sightAbility']),
+        int(request.form['smellAbility']),
+        int(request.form['hearingAbility']),
+        int(request.form['sightRange']),
+        int(request.form['smellRange']),
+        int(request.form['hearingRange']),
+        int(request.form['reactionTime']),
+        int(request.form['intelligence']),
+        int(request.form['selfPreservation']),
+        int(request.form['mobility']),
+        int(request.form['reproductionType']),
+        int(request.form['offspringAmount']),
+        int(request.form['motivation']),
+        int(request.form['maxEnergy']),
+        int(request.form['individualism']),
+        int(request.form['territorial']),
+        int(request.form['fightOrFlight']),
+        int(request.form['hostility']),
+        int(request.form['scent']),
+        int(request.form['stealth']),
+        int(request.form['lifeExpectancy']),
+        int(request.form['offensiveAbility']),
+        int(request.form['defensiveAbility']),
+        int(request.form['shape']),
+        int(request.form['color'])
+        )
+
+    return inputGenome
 
 
 @api.route('/get-info')
@@ -21,3 +67,34 @@ def return_dummy_info():
     }
 
     return response_body
+
+
+@api.route('/start-simulation')
+def startSimulation():
+    GOD = god.God()
+
+
+@api.route('/create-new-species', methods=['POST'])
+@cross_origin()
+def createNewSpecies():
+    initialGenome = _convertRequestToGenome(request)
+    GOD.createNewSpecies(request.form['speciesName'], initialGenome)
+
+
+@api.route('/delete-species', methods=['POST'])
+@cross_origin()
+def deleteSpecies():
+    GOD.deleteSpecies(request.form['speciesName'])
+
+
+@api.route('/create-new-creature', methods=['POST'])
+@cross_origin()
+def createNewCreature():
+    initialGenome = _convertRequestToGenome(request)
+    GOD.createNewCreature(request.form['speciesName'], initialGenome)
+    
+
+@api.route('/delete-creature', methods=['POST'])
+@cross_origin()
+def deleteCreature():
+    GOD.deleteCreature(request.form['speciesName'], request.form['creatureId'])
