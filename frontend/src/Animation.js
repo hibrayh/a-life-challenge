ef1f7af2f02058730733d15befdfd72520f22907
Homer78 <browninz@oregonstate.edu>
2023-02-12 12:45:16 -0800

    The start of retrieving food data from backend

--- frontend/src/Animation.js
+++ frontend/src/Animation.js
@@ -240,6 +240,109 @@ class Animation extends React.Component {
         })
     }
 
+    getEnvironmentInfo() {
+        // Use axios to retrieve info from the backend
+        axios({
+            method: 'GET',
+            url: 'http://localhost:5000/get-environment-info',
+        }).then((response) => {
+            const res = response.data
+            // change the state variable to trigger a re-render
+            this.setState({
+                foodName: res.foodName,
+                energyReplenishment: res.energyReplenishment,
+                rarity: res.rarity,
+                shape: res.shape,
+                color: res.color,
+                locationX: res.locationX,
+                locationY: res.locationY,
+            })
+            console.log(res)
+        })
+    }
+
+    async startSimulation() {
+        // Start the simulation
+        await axios({
+            method: 'GET',
+            url: 'http://localhost:5000/start-simulation',
+        })
+
+        await axios({
+            method: 'POST',
+            url: 'http://localhost:5000/create-new-species',
+            data: {
+                visibility: '0.5',
+                maxHealth: '0.5',
+                canSee: 'true',
+                canSmell: 'true',
+                canHear: 'true',
+                sightAbility: '0.5',
+                smellAbility: '0.5',
+                hearingAbility: '0.5',
+                sightRange: '0.5',
+                smellRange: '0.5',
+                hearingRange: '0.5',
+                reactionTime: '0.5',
+                intelligence: '0.5',
+                selfPreservation: '0.5',
+                mobility: '0.5',
+                reproductionType: 'sexual',
+                offspringAmount: '1',
+                motivation: '0.5',
+                maxEnergy: '0.5',
+                individualism: '0.5',
+                territorial: '0.5',
+                fightOrFlight: '0.5',
+                hostility: '0.5',
+                scent: '0.5',
+                stealth: '0.5',
+                lifeExpectancy: '0.5',
+                offensiveAbility: '0.5',
+                defensiveAbility: '0.5',
+                shape: 'circle',
+                color: 'red',
+                speciesName: 'Shlorpians',
+            },
+        })
+
+        await axios({
+            method: 'POST',
+            url: 'http://localhost:5000/mass-create-more-creatures',
+            data: {
+                speciesName: 'Shlorpians',
+                numberOfNewCreatures: '2',
+            },
+        })
+
+        await axios({
+            method: 'GET',
+            url: 'http://localhost:5000/advance-simulation',
+        })
+
+        await axios({
+            method: 'GET',
+            url: 'http://localhost:5000/get-simulation-info',
+        }).then((response) => {
+            const res = response.data
+            this.setState({
+                isSimStarted: true,
+                creatures: res.creatureRegistry,
+            })
+        })
+
+        await axios({
+            method: 'GET',
+            url: 'http://localhost:5000/get-environment-info',
+        }).then((response) => {
+            const res = response.data
+            this.setState({
+                isSimStarted: true,
+                foodObjects: res.foodRegistry,
+            })
+        })
+    }
+
     render() {
         // Example of looping through all creatures and animating
         let jsx = []
